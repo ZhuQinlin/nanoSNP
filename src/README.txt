47f0ac40a9d3c91cc211261374611e8ac2b7e249
ZhuQinlin <1090148573@qq.com>
2018-09-15 12:34:34 +0800

    README.txt changed

--- src/README.txt
+++ src/README.txt
@@ -9,3 +9,8 @@ integrate_eventalign_KF278742.1_dict.py：
     arg1: eventalign_names.tsv
 
     实际是my_anchor.py中的一个函数,用以整合eventalign_names.tsv中的信息到一个字典中，并将字典保存到一个文件“integrated_eventalign_KF278742.1_dict.txt”中，成功的话，以后my_anchor.py就不用读eventalign_names.tsv，而是从这个字典文件里得到需要的数据。函数尽可能地计算出打分时需要的所有信息,只留下有用的略去无用的,并以字典形式呈现,使后面的工作能够快捷方便地获取这些数据。问题在于太慢了。
+    最终跑出来用了11个小时左右。但是使用eval()函数转换输出结果文件为字典时，出现内存错误，应该是这个字符串太大了。
+
+integrate_eventalign_KF278742.1_format.py:
+    arg1: eventalign_names.tsv
+    与上面的程序类似，但是输出文件采用自己规定的格式，而不是dict的字符串表示方式，且是增量输出，运行时没有占用巨大内存的dict存在，事实证明好像是比上面快一点。
--- src/anchor.py
+++ src/anchor.py
@@ -1,6 +1,8 @@
 import sys
 import string
+import csv
 from itertools import imap, ifilter
+from scipy.stats import norm
 
 def fasta_read(f):
     name, seq = '', ''
@@ -21,7 +23,6 @@ def fasta_read(f):
                 seq += line
     if len(seq) > 0:
         yield name, seq
-
 
 def seq_warping(seq, alignform):
     count = 0
@@ -84,14 +85,67 @@ def seq_condicate(alignform):
             count = 0
     return condicates
 
+def score(data, mean, stdv):
+    x = (data-mean)/stdv
+    delta = norm.ppf(0.9)
+    return norm.cdf(x+delta) - norm.cdf(x-delta)
+
+def integrate_eventalign(path):
+    reader = csv.reader(open(path), delimiter = '\t')
+    position = -1
+    count = 0
+    summary = []
+    product = 1
+    for row in reader:
+        if not row[3].isdigit():
+            continue
+        if row[3] == '0':
+            if int(row[1]) == position:
+                count = count+1
+                event_level_mean = float(row[6])
+                model_mean = float(row[10])
+                model_stdv = float(row[11])
+                if row[9] == 'NNNNNN':
+                    product = 0
+                else:
+                    product = product * score(event_level_mean, model_mean, model_stdv)
+            else:
+                if not position == -1:
+                    summary.append([position, count, product])
+                if int(row[1]) > position + 1:
+                    position = position + 1
+                    while position < int(row[1]):
+                        summary.append([position, 0, 0])
+                        position = position + 1
+                position = int(row[1])
+                count = 1
+                if row[9] == 'NNNNNN':
+                    product = 0
+                else:
+                    event_level_mean = float(row[6])
+                    model_mean = float(row[10])
+                    model_stdv = float(row[11])
+                    product = score(event_level_mean, model_mean, model_stdv)
+        else:
+            summary.append([position, count, product])
+            break
+    return summary
+
 def get_events(f):
     pass
 
 def get_eventAlign(f):
     pass
 
-def candicate_socre(candicate_range):
-    pass
+def condicate_score(condicate_range, eventalign):
+    count = 0
+    score = 1
+    i = condicate_range[0]
+    while i < condicate_range[1]:
+        score = score * eventalign[i][2]
+        count = count + eventalign[i][1]
+        i = i+1
+    return score**(1.0/count)
 
 def signal_split(eventAlign):
     pass
@@ -111,12 +165,19 @@ if __name__ == '__main__':
     with file(sys.argv[2]) as f:
         for label, refName, pos, alignform, seq in get_SAM(f):
             samData.append((label, refName, pos, alignform, seq))
+    integrated_eventalign = integrate_eventalign(sys.argv[3])
     for sam in samData:
         label, refName, pos, alignform, seq = sam
         if refName == 'KF278742.1':
-            wseq = seq_warping(seq, alignform)
-            condicates = seq_condicate(alignform)
+            #wseq = seq_warping(pos, seq, alignform)
+            #condicates = seq_condicate(alignform)
+            wseq, condicates = seq_warp_condicates(pos, seq, contigs[refName], alignform)
             print(contigs[refName][pos - 1: pos + len(wseq)])
             print(wseq)
             print(condicates)
-            break
+            scores = []
+            for pair in condicates:
+                temp = condicate_score(pair, integrated_eventalign)
+                scores.append(temp)
+            print(scores)
+            print '\n\n\n'
